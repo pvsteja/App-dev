e5ea96b930ab66e88cba784b7e0fc51899a64bc7
pvsteja <tejapathri6@gmail.com>
2020-04-20 12:26:48 +0530

    orm completed

--- Week-2/project1/create.py
+++ Week-2/project1/create.py
@@ -4,11 +4,10 @@ from flask import Flask, render_template, request
 from models import *
 
 app = Flask(__name__)
-os.environ["DATABASE_URL"] = 
+os.environ["DATABASE_URL"] = "postgres://wwgchdfoipxweb:6f944eb3a2278561cc5e9ffe3144e89104f144429ec4f7b87cce9ffa0c0e3a66@ec2-54-197-48-79.compute-1.amazonaws.com:5432/d48jjugm82ikeu"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-
 def main():
     db.create_all()
 
--- Week-2/project1/models.py
+++ Week-2/project1/models.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-class users(db.model):
+class users(db.Model):
 	__tablename__ = "usercredentials"
 	user = db.Column(db.String, primary_key=True)
 	password = db.Column(db.String, nullable=False)
-	time = db.Column(db.Datetime(), nullable=False)
+	time = db.Column(db.DateTime(), nullable=False)
 
 	def __init__(self, user, password, time):
-	self.user = user
-	self.password = password
-	self.time = time
+		self.user = user
+		self.password = password
+		self.time = time
